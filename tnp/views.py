34ef79616abdca4c62633187893679ea3ccd7a91
Fabian Monheim <fabian.monheim@leibniz-fli.de>
2022-02-24 16:27:29 +0100

    added admin view

--- tnp/views.py
+++ tnp/views.py
@@ -15,7 +15,7 @@ from django.views import generic
 
 def send_info_mail_to_tec_admin(e,procedure):
     tec_admin_mail = getattr(settings, "TEC_ADMIN_EMAIL", None)
-    #send_mail("Error Sendungsregister","Sendungsregister error {} in procedure {} in line {} ".format(e,procedure,sys.exc_info()[2].tb_lineno) , "sendungsregister@leibniz-fli.de",[tec_admin_mail])
+    send_mail("Error Sendungsregister","Sendungsregister error {} in procedure {} in line {} ".format(e,procedure,sys.exc_info()[2].tb_lineno) , "sendungsregister@leibniz-fli.de",[tec_admin_mail])
 
 def send_info_mail_about_new_user(user):
     tec_admin_mail = getattr(settings, "TEC_ADMIN_EMAIL", None)
@@ -27,7 +27,10 @@ def overview(request):
     try:
         profile = get_object_or_404(Profile, user=request.user)
         if profile.lab:
-            entries = Dataset.objects.filter(lab=profile.lab)
+            if profile.lab.name == "Admin":
+                entries = Dataset.objects.all()
+            else:
+                entries = Dataset.objects.filter(lab=profile.lab)
             f = DatasetFilter(request.GET, queryset=entries)
             return render(request, 'home.html', {'filter': f})
         else:
